6c062ccab62d160adb19e26ed15cb036b347cb37
Matt Lajoie <mlajoie03@gmail.com>
2018-03-17 23:17:40 -0400

    Added i10n translator Added app-component generic outlook other stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Language, LocaleService} from 'angular-l10n';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+    @Language() lang: string;
+    private menuOpen = true;
+    constructor(public locale: LocaleService) {}
+
+    selectLanguage(language: string): void {
+        this.locale.setCurrentLanguage(language);
+    }
+
+    getOpen(): boolean {
+        return this.menuOpen;
+    }
+
+    switchOpen(): void {
+        this.menuOpen = !this.menuOpen;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {L10nConfig, L10nLoader, ProviderType, StorageStrategy, TranslationModule} from 'angular-l10n';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClient, HttpClientModule, HttpHandler} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
 
+const l10nConfig: L10nConfig = {
+    locale: {
+        languages: [
+            {code: 'en', dir: 'ltr'},
+            {code: 'fr', dir: 'ltr'}
+        ],
+        language: 'en',
+        storage: StorageStrategy.Cookie
+    },
+    translation: {
+        providers: [
+            {type: ProviderType.Static, prefix: './assets/locale-'}
+        ],
+        caching: true,
+        missingValue: 'No key found'
+    }
+};
+
+const appRoutes: Routes = [
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        TranslationModule.forRoot(l10nConfig),
+        HttpClientModule,
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(public l10nLoader: L10nLoader) {
+        this.l10nLoader.load();
+    }
+}
